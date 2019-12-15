66cf1c56aad1e9f4a5ae1e6d6c78ab54b3004565
Timur Badretdinov <destinerx@gmail.com>
2019-12-15 16:33:35 +0300

    Save Chai supply

--- src/chai.ts
+++ src/chai.ts
@@ -1,7 +1,7 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 
 import { Transfer } from "../generated/Chai/Chai";
-import { User } from "../generated/schema";
+import { Chai, User } from "../generated/schema";
 
 let zeroAddress = Address.fromString('0x0000000000000000000000000000000000000000');
 
@@ -10,6 +10,23 @@ export function handleTransfer(event: Transfer): void {
 	let from = event.params.src;
 	let to = event.params.dst;
 	let wad = event.params.wad;
+
+	let chai = Chai.load('0');
+	if (!chai) {
+		chai = new Chai('0');
+		chai.supply = new BigInt(0);
+	}
+
+	if (from == zeroAddress) {
+		// Mint
+		chai.supply += wad;
+	}
+
+	if (to == zeroAddress) {
+		// Burn
+		chai.supply -= wad;
+	}
+	chai.save();
 
 	if (from != zeroAddress) {
 		let user = User.load(from.toHexString());
