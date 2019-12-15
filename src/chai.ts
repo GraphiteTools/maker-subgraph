0b83d3be73e00ae2ae052b9df4bed0a5d1747531
Timur Badretdinov <destinerx@gmail.com>
2019-12-15 17:48:20 +0300

    Save ChaiTransfer events

--- src/chai.ts
+++ src/chai.ts
@@ -1,12 +1,15 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 
 import { Transfer } from "../generated/Chai/Chai";
-import { Chai, User } from "../generated/schema";
+import { Chai, User, ChaiTransferEvent } from "../generated/schema";
 
 let zeroAddress = Address.fromString('0x0000000000000000000000000000000000000000');
 
 export function handleTransfer(event: Transfer): void {
-
+	let blockNumber = event.block.number;
+	let blockTime = event.block.timestamp;
+	let transactionHash = event.transaction.hash;
+	let eventIndex = event.logIndex;
 	let from = event.params.src;
 	let to = event.params.dst;
 	let wad = event.params.wad;
@@ -27,6 +30,14 @@ export function handleTransfer(event: Transfer): void {
 		chai.supply -= wad;
 	}
 	chai.save();
+
+	let eventId = transactionHash.toHexString() + '-' + eventIndex.toString();
+	let event = new ChaiTransferEvent(eventId);
+	event.blockNumber = blockNumber;
+	event.blockTime = blockTime;
+	event.from = from;
+	event.to = to;
+	event.amount = wad;
 
 	if (from != zeroAddress) {
 		let user = User.load(from.toHexString());
