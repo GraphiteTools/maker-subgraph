90b07cf54f156290fc557ccda2150471937e4e2c
Timur Badretdinov <destinerx@gmail.com>
2019-12-16 13:56:02 +0300

    Handle Jug

---
+++ src/jug.ts
@@ -0,0 +1,40 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
+import { FileCall, File1Call } from "../generated/Jug/Jug";
+import { Jug, Collateral } from "../generated/schema";
+
+export function handleDutyFile(call: FileCall): void {
+	let ilk = call.inputs.ilk;
+	let what = call.inputs.what;
+	let data = call.inputs.data;
+	if (what.toString() != 'duty') {
+		return;
+	}
+
+	let collateral = new Collateral(ilk.toString());
+	if (!collateral) {
+		collateral = new Collateral(ilk.toString());
+
+		let jug = new Jug('0');
+		jug.collaterals.push(ilk.toString());
+		jug.save();
+	}
+	collateral.rate = data;
+	collateral.save();
+}
+
+export function handleBaseFile(call: File1Call): void {
+	let what = call.inputs.what;
+	let data = call.inputs.data;
+	if (what.toString() != 'base') {
+		return;
+	}
+
+	let jug = new Jug('0');
+	if (!jug) {
+		jug.baseRate = new BigInt(0);
+		jug.collaterals = [];
+	}
+	jug.baseRate = data;
+	jug.save();
+}
